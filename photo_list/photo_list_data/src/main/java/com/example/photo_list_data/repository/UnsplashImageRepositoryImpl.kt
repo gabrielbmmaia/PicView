2681cc048121b24ff6227ff156f5420c265a0d87
Maia <gbmmaia.contato@gmail.com>
2023-05-11 14:28:28 -0300

    fix (photo_list_data): Função de getAllImages de UnsplashImageRepositoryImpl refatorado para utilizar o UnsplashPagingSource

--- photo_list/photo_list_data/src/main/java/com/example/photo_list_data/repository/UnsplashImageRepositoryImpl.kt
+++ photo_list/photo_list_data/src/main/java/com/example/photo_list_data/repository/UnsplashImageRepositoryImpl.kt
@@ -1,11 +1,16 @@
 package com.example.photo_list_data.repository
 
-import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import androidx.paging.map
+import com.example.photo_list_data.mappers.toUnsplashImage
+import com.example.photo_list_data.paging.UnsplashPagingSource
 import com.example.photo_list_data.remote.UnsplashApi
 import com.example.photo_list_domain.model.UnsplashImage
 import com.example.photo_list_domain.repository.UnsplashImageRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 
@@ -14,6 +19,13 @@ class UnsplashImageRepositoryImpl @Inject constructor(
 ) : UnsplashImageRepository {
 
     override fun getAllImages(): Flow<PagingData<UnsplashImage>> {
-        TODO()
+        val pagingSourceFactory = { UnsplashPagingSource(unsplashApi = unsplashApi) }
+        val pager = Pager(
+            config = PagingConfig(pageSize = UnsplashApi.PER_PAGE),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow
+        return pager.map { pagingData ->
+            pagingData.map { it.toUnsplashImage() }
+        }
     }
 }
