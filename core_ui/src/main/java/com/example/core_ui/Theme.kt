bd57a29ba7f2faa8ef3f652131aa3624973fc352
Maia <gbmmaia.contato@gmail.com>
2023-06-02 12:50:23 -0300

    fix (app) Alterado cores da status bar para ficar sempre Color.Black com conteudo Color.White independente do tema utilizado

--- core_ui/src/main/java/com/example/core_ui/Theme.kt
+++ core_ui/src/main/java/com/example/core_ui/Theme.kt
@@ -84,7 +84,7 @@ fun PicViewTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = Color.Black.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
     }
 
